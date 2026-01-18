1c03b9d378f73fed0974318c8064374912250295
Najib Ishaq <najib_ishaq@zoho.com>
2026-01-18 14:21:12 -0500

    better plots for polar distances

--- papers/musals/py/src/paper_musals/plots/polar_distances.py
+++ papers/musals/py/src/paper_musals/plots/polar_distances.py
@@ -8,8 +8,9 @@ import time
 
 import lmfit
 import numpy
+import pandas
+import plotly.express as px
 import plotly.graph_objects as go
-import plotly.io as pio
 import pydantic
 import typer
 from plotly.subplots import make_subplots
@@ -43,18 +44,17 @@ def project_distances(l_distances: numpy.ndarray, r_distances: numpy.ndarray, ba
     """
     l_distances = l_distances.astype(numpy.float64)
     r_distances = r_distances.astype(numpy.float64)
-    projection = numpy.zeros_like(l_distances, dtype=numpy.float64)
-    for i, (left, right) in enumerate(zip(l_distances, r_distances, strict=True)):
-        d: numpy.float64
-        if left > base and right > base:
-            d = project_triangle(left, right, base)
-        elif left > base:
-            d = -project_triangle(left, right, base)
-        elif right > base:
-            d = project_triangle(left, right, base)
-        else:
-            d = project_triangle(left, right, base)
-        projection[i] = d
+
+    # Compute the height of the triangle using Heron's formula
+    s = (l_distances + r_distances + base) / 2.0
+    area = numpy.sqrt(s * (s - l_distances) * (s - r_distances) * (s - base))
+    h = (2.0 * area) / base
+
+    # Compute the projection and preserve the sign using the law of cosines
+    cos_l = numpy.square(l_distances) + numpy.square(base) - numpy.square(r_distances)
+    projection = numpy.sqrt(numpy.square(l_distances) - numpy.square(h))
+    projection *= numpy.sign(cos_l)
+
     return projection
 
 
@@ -78,22 +78,33 @@ def project_triangle(left: numpy.float64, right: numpy.float64, base: float) -> 
 def fit_gaussian_thread(
     l_distances: numpy.ndarray,
     r_distances: numpy.ndarray,
-    base: numpy.float64,
-) -> tuple[float, float, float, float]:
+    annotation: tuple[int, int] | None,
+    depth: int,
+    cardinality: int,
+) -> tuple[go.Scatter, float, int, int] | None:
     """Fit a Gaussian to the projected polar distances in a separate thread.
 
     Args:
         l_distances: Distances from the left pole.
         r_distances: Distances from the right pole.
-        base: Distance between the two poles.
+        annotation: The annotation tuple containing (span, inter-polar distance).
+        depth: The depth of the cluster.
+        cardinality: The cardinality of the cluster.
 
     Returns:
         A tuple containing:
-            - fitted_amplitude: The amplitude of the fitted Gaussian.
-            - fitted_mean: The mean of the fitted Gaussian.
-            - fitted_stddev: The standard deviation of the fitted Gaussian.
-            - r2: The R² score of the fit.
+            - trace: The Plotly Scatter trace representing the fitted Gaussian.
+            - r2_score: The R² score of the fit.
+            - depth: The depth of the cluster.
+            - cardinality: The cardinality of the cluster.
     """
+    if annotation is None:
+        return None
+
+    base = numpy.float64(annotation[1])
+    if base < 1e-5:  # noqa: PLR2004
+        return None  # Skip if the base distance is too small
+
     projection = project_distances(l_distances, r_distances, base) / base
 
     # use numpy to calculate histogram
@@ -104,6 +115,9 @@ def fit_gaussian_thread(
     amplitude_init = numpy.max(counts)
     mean_init = numpy.mean(projection)
     stddev_init = numpy.std(projection)
+
+    if stddev_init < 1e-5:  # noqa: PLR2004
+        return None  # Skip if the initial stddev is too small
 
     # Define the Gaussian model using lmfit
     model = lmfit.Model(gaussian)
@@ -119,11 +133,26 @@ def fit_gaussian_thread(
     fitted_mean = result.params["mean"].value
     fitted_stddev = result.params["stddev"].value
 
+    if fitted_amplitude < 1e-5 or fitted_amplitude > 1e6:  # noqa: PLR2004
+        return None  # Skip if the fitted amplitude is too high or too low
+
+    if fitted_stddev < 1e-5:  # noqa: PLR2004
+        return None  # Skip if the fitted stddev is too small
+
     # Calculate R^2 to assess the goodness of fit
     fitted_counts = gaussian(midpoints, fitted_amplitude, fitted_mean, fitted_stddev)
     r2 = r2_score(counts, fitted_counts)
 
-    return fitted_amplitude, fitted_mean, fitted_stddev, r2
+    left, right = fitted_mean - fitted_stddev, fitted_mean + fitted_stddev
+    trace = go.Scatter(
+        x=[left, fitted_mean, right],
+        y=[fitted_amplitude, fitted_amplitude, fitted_amplitude],
+        mode="lines",
+        name=f"R²={r2:.3f}",
+        line=dict(width=1),  # noqa: C408
+    )
+
+    return trace, r2, depth, cardinality
 
 
 def polar_distances(  # noqa: PLR0915
@@ -165,16 +194,7 @@ def polar_distances(  # noqa: PLR0915
     end = time.perf_counter() - start
     typer.echo(f"Read polar distances from {npz_path} in {end:.2f} seconds")
 
-    # Create a figure with two subplots
-    fig = make_subplots(
-        rows=2,
-        cols=1,
-        row_heights=[0.85, 0.15],
-        subplot_titles=("Polar Distances Projections", "R² Histogram"), # pyright: ignore[reportArgumentType]
-        vertical_spacing=0.1,
-    )
-
-    min_car, max_car = 10, 20_000_000
+    min_car, max_car = 10, 2_000_000
     clusters = [
         c for c in clusters if all(
             [
@@ -186,16 +206,15 @@ def polar_distances(  # noqa: PLR0915
     ]
     typer.echo(f"Filtered clusters to {len(clusters)} with cardinality in ({min_car}, {max_car})")
 
-    params_results = []
+    traces: list[go.Scatter] = []
+    r2_scores: list[float] = []
+    depths: list[int] = []
+    cardinalities: list[int] = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = []
         for cluster in clusters:
             if cluster.annotation is None:
                 continue
-
-            base = numpy.float64(cluster.annotation[1])
-            if base < 1e-5:  # noqa: PLR2004
-                continue  # Skip clusters with zero base distance
 
             l_name = f"{cluster.center_index}_l"
             r_name = f"{cluster.center_index}_r"
@@ -209,49 +228,54 @@ def polar_distances(  # noqa: PLR0915
                     fit_gaussian_thread,
                     l_distances,
                     r_distances,
-                    base,
+                    cluster.annotation,
+                    cluster.depth,
+                    cluster.cardinality,
                 ),
             )
 
         for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Fitting Gaussians"):
-            params_results.append(future.result())  # noqa: PERF401
+            result = future.result()
+            if result is None:
+                continue
+            trace, r2_score, depth, cardinality = result
+            traces.append(trace)
+            r2_scores.append(r2_score)
+            depths.append(depth)
+            cardinalities.append(cardinality)
 
-    r2_scores = []
-    amplitudes = []
-    for params in params_results:
-        fitted_amplitude, fitted_mean, fitted_stddev, r2 = params
-        # If the fitted amplitude is too high, skip it
-        if numpy.log10(fitted_amplitude) > 20:  # noqa: PLR2004
-            continue
+    typer.echo(f"Fitted Gaussians to {len(traces)} clusters")
 
-        amplitudes.append(fitted_amplitude)
-        r2_scores.append(r2)
+    # Create a figure with two subplots
+    fig1 = make_subplots(
+        rows=2,
+        cols=1,
+        row_heights=[0.85, 0.15],
+        subplot_titles=("Polar Distances Projections", "R² Histogram"), # pyright: ignore[reportArgumentType]
+        vertical_spacing=0.1,
+    )
 
-        # Add a horizontal line to represent one standard deviation from the mean
-        left, right = fitted_mean - fitted_stddev, fitted_mean + fitted_stddev
-        fig.add_trace(
-            go.Scatter(
-                x=[left, fitted_mean, right],
-                y=[fitted_amplitude, fitted_amplitude, fitted_amplitude],
-                mode="lines",
-                name=f"R²={r2:.3f}",
-            ),
-            row=1,
-            col=1,
-        )
+    # Hide the legend
+    fig1.update_layout(showlegend=False)
 
-    # Make the lines thinner for better visibility, and set the x-axis range to (0, 1)
-    fig.update_traces(line=dict(width=1), row=1, col=1)  # noqa: C408
-    fig.update_layout(xaxis=dict(range=[-0.1, 1.1]))  # noqa: C408
+    # Set the x-axis range for both subplots
+    fig1.update_xaxes(dict(range=[-0.05, 1.05]), row=1, col=1)  # noqa: C408
+    fig1.update_xaxes(dict(range=[-0.05, 1.05]), row=2, col=1)  # noqa: C408
 
-    # Set the y-axis range
-    amp_percentile = numpy.percentile(amplitudes, 99)  # 99th percentile
-    fig.update_layout(yaxis=dict(range=[1, amp_percentile * 2.0]))  # noqa: C408
+    # Set appropriate titles and labels for the axes
+    fig1.update_xaxes(title_text="Projected Distance (normalized)", row=1, col=1)
+    fig1.update_xaxes(title_text="R² Score", row=2, col=1)
+    fig1.update_yaxes(title_text="Amplitude", type="log", row=1, col=1)
+    fig1.update_yaxes(title_text="# Clusters", row=2, col=1)
+
+    # Add traces to the first subplot
+    for trace in tqdm(traces, desc="Adding traces to figure"):
+        fig1.add_trace(trace, row=1, col=1)
 
     # Add a histogram of the R^2 scores to the second subplot
-    counts, bin_edges = numpy.histogram(r2_scores)
+    counts, bin_edges = numpy.histogram(r2_scores, bins=100, range=(0.0, 1.0))
     midpoints = (bin_edges[1:] + bin_edges[:-1]) / 2.0
-    fig.add_trace(
+    fig1.add_trace(
         go.Scatter(
             x=midpoints,
             y=counts,
@@ -262,21 +286,56 @@ def polar_distances(  # noqa: PLR0915
         col=1,
     )
 
-    # Set appropriate titles and labels for the axes
-    fig.update_xaxes(title_text="Projected Distance (normalized)", row=1, col=1)
-    fig.update_yaxes(title_text="Counts", row=1, col=1)
-    fig.update_xaxes(title_text="R² Score", row=2, col=1)
-    fig.update_yaxes(title_text="# Clusters", row=2, col=1)
+    plot_path = plots_dir / "polar_distances.html"
+    typer.echo(f"Saving polar distances plot to {plot_path} with html format")
+    fig1.write_html(plot_path)
 
-    # Hide the legend if there are too many entries
-    if len(fig.data) > 10:  # noqa: PLR2004
-        typer.echo("Hiding legend due to too many entries")
-        fig.update_layout(showlegend=False)
+    # Create a pandas DataFrame for easier handling for the second plot
+    explorations_df = pandas.DataFrame({
+        "R2": r2_scores,
+        "Depth": depths,
+        "Cardinality": cardinalities,
+    })
 
-    plot_path = plots_dir / "polar_distances"
-    typer.echo(f"Saving polar distances plot to {plot_path} with html and png formats")
-    pio.write_image(fig, plot_path.with_suffix(".png"), height=1000, width=1600, scale=4)
-    fig.write_html(plot_path.with_suffix(".html"))
+    # Create a second figure for scatter plots of depth and cardinality vs R²
+    fig2 = make_subplots(
+        rows=1,
+        cols=2,
+        subplot_titles=("Depth vs R²", "Cardinality vs R²"), # pyright: ignore[reportArgumentType]
+        horizontal_spacing=0.075,
+    )
+    fig2.update_xaxes(title_text="R² Score", row=1, col=1)
+    fig2.update_xaxes(title_text="R² Score", row=1, col=2)
+    fig2.update_yaxes(title_text="Depth", row=1, col=1)
+    fig2.update_yaxes(title_text="Cardinality", row=1, col=2)
 
+    fig2a = px.scatter(
+        explorations_df,
+        x="R2",
+        y="Depth",
+        color="Cardinality",
+        color_continuous_scale="bluered",
+    )
+    fig2a.update_traces(marker=dict(size=3))  # noqa: C408
+
+    fig2b = px.scatter(
+        explorations_df,
+        x="R2",
+        y="Cardinality",
+        color="Depth",
+        color_continuous_scale="bluered",
+    )
+    fig2b.update_traces(marker=dict(size=3))  # noqa: C408
+    for trace in fig2a.data:
+        fig2.add_trace(trace, row=1, col=1)
+    for trace in fig2b.data:
+        fig2.add_trace(trace, row=1, col=2)
+
+    # Set the y-axis to logarithmic scale for cardinality plot
+    fig2.update_yaxes(type="log", row=1, col=2)
+
+    plot_path2 = plots_dir / "r2_scatter.html"
+    typer.echo(f"Saving R² scatter plots to {plot_path2} with html format")
+    fig2.write_html(plot_path2)
 
 __all__ = ["polar_distances"]
